6f857a7a7e9610099de11da548229362cc28d380
APixelOcean <apixelocean@gmail.com>
2024-12-07 23:50:17 -0500

    Simulation block

---
+++ src/routes/components/Simulation.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let id = -1;
+  export let href = '/';
+  export let title = '';
+  export let imgPath = '';
+  export let imgDesc = ''
+  export let description = '';
+</script>
+
+<a class="simulation" id={id} href={href}>
+  <div class="simulation-title">{@html title}</div>
+  <div class="simulation-img">
+    <img src={imgPath} alt={imgDesc}>
+  </div>
+  <div class="simulation-desc">{description}</div>
+</a>
